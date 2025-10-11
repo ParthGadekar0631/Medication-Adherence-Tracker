28e037b2aba635dd4dc0c09440626ee75f4750e8
ParthGadekar0631 <parthgadekar060202@gmail.com>
2025-10-11 16:45:32 -0400

    Backend Files and controllers

---
+++ backend/src/config/firebase.js
@@ -0,0 +1,16 @@
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+import { readFileSync } from "fs";
+
+dotenv.config();
+
+const serviceAccount = JSON.parse(
+  readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, "utf8")
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export const db = admin.firestore();
+export const auth = admin.auth();
