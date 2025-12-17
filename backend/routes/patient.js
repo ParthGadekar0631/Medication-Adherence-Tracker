4a7815c651830f102377feccf0042472a5badee6
ParthGadekar0631 <parthgadekar060202@gmail.com>
2025-12-16 23:34:32 -0500

    removing corrupt file

--- backend/routes/patient.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { getAssignedProviders } = require('../controllers/patientController');
-
-router.get('/providers', protect, getAssignedProviders);
-
-module.exports = router;
